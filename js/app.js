3c202ab27883b24bcf7d0fff8d5df5f6876e5b2d
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2025-01-25 14:19:59 -0300

    implementa a função verificaQtdeJogosAlugados()

--- js/app.js
+++ js/app.js
@@ -4,12 +4,26 @@ function alterarStatus(id) {
     let imagemGame = gameClicado.querySelector(".dashboard__item__img");
     let nomeGame = gameClicado.querySelector(".dashboard__item__name").textContent;
 
-    if(imagemGame.classList.contains("dashboard__item__img--rented")){
+    if (imagemGame.classList.contains("dashboard__item__img--rented")) {
         confirm(`Tem certeza que irá devolver o jogo "${nomeGame}"?`);
     }
 
     botaoGame.classList.toggle("dashboard__item__button--return");
     imagemGame.classList.toggle("dashboard__item__img--rented");
     botaoGame.textContent = botaoGame.textContent === "Alugar" ? "Devolver" : "Alugar";
+
+    verificaQtdeJogosAlugados();
+}
+
+function verificaQtdeJogosAlugados() {
+    let todosOsGames = document.querySelectorAll(".dashboard__item__img");
+    let listaDeTodosOsGames = Array.from(todosOsGames);
+    let contador = 0;
+
+    for (let i = 0; i < listaDeTodosOsGames.length; i++) {
+        if (listaDeTodosOsGames[i].classList.contains("dashboard__item__img--rented")) contador++;
+    }
+    let mensagem = contador > 1 ? "jogos alugados" : "jogo alugado";
+    console.log(`Há ${contador} ${mensagem}.`);
 }
 
