0e48279b94905bbcf0ff8eacd4c6ad66f6945b2b
André Alexandre Stella <andre.alexandre.stella@gmail.com>
2025-01-25 13:43:05 -0300

    confirma a devolucao do jogo antes dela ser concluida

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let botaoGame = gameClicado.querySelector(".dashboard__item__button");
     let imagemGame = gameClicado.querySelector(".dashboard__item__img");
+    let nomeGame = gameClicado.querySelector(".dashboard__item__name").textContent;
+
+    if(imagemGame.classList.contains("dashboard__item__img--rented")){
+        confirm(`Tem certeza que irá devolver o jogo "${nomeGame}"?`);
+    }
 
     botaoGame.classList.toggle("dashboard__item__button--return");
     imagemGame.classList.toggle("dashboard__item__img--rented");
